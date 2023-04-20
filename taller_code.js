a3e43a6b1dc7e6924620043d05f77bf420409e1d
francisco garrido <fg0611@gmail.com>
2023-04-20 11:12:26 -0300

    solved

--- taller_code.js
+++ taller_code.js
@@ -1,23 +1,38 @@
-// Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+//github.com/ldiazssj3/js-challenge/blob/main/README.md
 
-const array = [-10, -5, 0, 5, 10];
-const arraySquared = array
+// Write a function that takes in a non-empty array of integers that are sorted in ascending
+// order and returns a new array of the same length with the squares of the original integers also sorted in ascending order.
+
+const arr = [-10, -5, 0, 5, 10];
+const arrSquared = arr
   .map((i) => i * i)
   .sort(function (a, b) {
     return a - b;
   });
 
-console.log(arraySquared);
+console.log(arrSquared);
 
-//For example, if you’re given coins = [1, 2, 5], the minimum amount of change that you can’t create is 4. If you’re given no coins, the minimum amount of change that you can’t create is 1.
+// Given an array of positive integers representing the values of coins in your possession, write a function
+// that returns the minimum amount of change (the minimum sum of money) that you CANNOT create.
+// The given coins can have any positive integer value and aren't necessarily unique (i.e., you can have multiple coins of the same value).
 
-const coins = [1, 2, 5];
-let sum = 1;
+//For example, if you’re given coins = [1, 2, 5], the minimum amount of change that you can’t create is 4. If you’re given no coins,
+// the minimum amount of change that you can’t create is 1.
 
-coins.forEach((c) => {
-  if ((sum = c)) {
-    sum = +c;
-  } else {
-    return;
-  }
-});
+const coins1 = [1, 2, 5];
+const coins2 = [5, 7, 1, 1, 2, 3, 22];
+
+const calChange = (coinsArr) => {
+  if (!coinsArr?.length) throw "coins array can't be empty";
+  let sum = 1;
+  coinsArr.forEach((c) => {
+    if (c <= sum) {
+      console.log("ok: ", sum);
+      sum += c;
+    } else {
+      throw `couldn't calculate change for value = ${sum}`;
+    }
+  });
+};
+
+calChange(coins2);
